8c8818f0468c094dfc214a090f3f4e0ad87aad3c
Xin <lovemarks19@gmail.com>
2017-11-02 10:55:48 +0800

    修改主樣板內 script 標籤 js 檔案的參考位置，利用 asset() 輔助方法

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,3 +4,13 @@
 <link href="{{asset('css/clean-blog.min.css')}}" rel="stylesheet">
 <!-- Custom Fonts -->
 <link href="{{asset('css/font-awesome.min.css')}}" rel="stylesheet" type="text/css">
+
+<!-- jQuery -->
+<script src="{{asset('js/jquery.min.js')}}"></script>
+<!-- Bootstrap Core JavaScript -->
+<script src="{{asset('js/bootstrap.min.js')}}"></script>
+<!-- Contact Form JavaScript -->
+<script src="{{asset('js/jqBootstrapValidation.js')}}"></script>
+<script src="{{asset('js/contact_me.js')}}"></script>
+<!-- Theme JavaScript -->
+<script src="{{asset('js/clean-blog.min.js')}}"></script>
